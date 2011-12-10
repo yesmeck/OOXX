c18bf4a8730bcf1bb766fd263d085ae9894bfba6
Meck <yesmeck@gmail.com>
2011-12-10 17:29:36 +0800

    调整登录表单
    ZF的表单实在是太复杂了!

--- application/forms/User/Login.php
+++ application/forms/User/Login.php
@@ -9,11 +9,34 @@ class Application_Form_User_Login extends Zend_Form
              ->setMethod('post')
              ->setAttrib('id', 'user-login-form')
              ->setDisableLoadDefaultDecorators(true);
-
+ 
         $this->setDecorators(array(
             array('ViewScript', array('viewScript' => 'user/form/_login.phtml')),
-            'Form'
+            'Form',
         ));
+
+        $email = new Zend_Form_Element_Text('email');
+        $email->setOptions(array('class' => 'xlarge'))
+              ->removeDecorator('Errors')
+              ->removeDecorator('Description')
+              ->removeDecorator('HtmlTag')
+              ->removeDecorator('Label');
+
+        $password = new Zend_Form_Element_Password('password');
+        $password->setOptions(array('class' => 'xlarge'))
+                 ->removeDecorator('Errors')
+                 ->removeDecorator('Description')
+                 ->removeDecorator('HtmlTag')
+                 ->removeDecorator('Label');
+
+        $submit = new Zend_Form_Element_Submit('submit');
+        $submit->setLabel('登录')
+               ->setOptions(array('class' => 'btn primary'))
+               ->removeDecorator('DtDdWrapper');
+
+        $this->addELement($email)
+             ->addELement($password)
+             ->addELement($submit);
     }
 
 
