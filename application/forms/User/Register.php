3f654b314507bd5144b84828b9d3be2445196081
Meck <yesmeck@gmail.com>
2011-12-11 13:39:11 +0800

    调整注册表单

--- application/forms/User/Register.php
+++ application/forms/User/Register.php
@@ -1,21 +1,40 @@
 <?php
 
-class Application_Form_User_Register extends Zend_Form
+class Application_Form_User_Register extends Application_Form_User_Login
 {
 
     public function init()
     {
+        parent::init();
+
         $this->setAction('/user/register')
-             ->setMethod('post')
-             ->setAttrib('id', 'user-register-form')
-             ->setDisableLoadDefaultDecorators(true);
+             ->setAttrib('id', 'user-register-form');
 
         $this->setDecorators(array(
             array('ViewScript', array('viewScript' => 'user/form/_register.phtml')),
             'Form'
         ));
+
+
+        $password2 = new Zend_Form_Element_Password('password2');
+        $password2->setOptions(array('class' => 'xlarge'))
+                  ->removeDecorator('Errors')
+                  ->removeDecorator('Description')
+                  ->removeDecorator('HtmlTag')
+                  ->removeDecorator('Label');
+
+        $nickname = new Zend_Form_Element_Text('nickname');
+        $nickname->setOptions(array('class' => 'xlarge'))
+                 ->removeDecorator('Errors')
+                 ->removeDecorator('Description')
+                 ->removeDecorator('HtmlTag')
+                 ->removeDecorator('Label');
+
+
+        $this->getElement('submit')->setLabel('登录');
+
+        $this->addELement($password2)
+             ->addELement($nickname);
     }
-
-
 }
 
