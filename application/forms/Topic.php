ca498f788c13d6974c16cc6f2bf909643a820bcf
Meck <yesmeck@gmail.com>
2011-12-28 22:40:21 +0800

    Make code looks better

--- application/forms/Topic.php
+++ application/forms/Topic.php
@@ -2,38 +2,34 @@
 
 class Application_Form_Topic extends OOXX_Form
 {
-
     public function init()
     {
         $this->setAction('/topic/new')
              ->setMethod('post')
              ->setAttrib('id', 'new-topic-form');
 
-        $elements = array();
+        $this->addELements(array(
+            $this->createElement('text', 'title')
+                 ->setLabel('标题')
+                 ->setDescription('，你们有什么想问的？')
+                 ->addDecorator('Description', array('tag' => ''))
+                 ->setOptions(array('class' => 'xlarge'))
+                 ->setRequired(true),
 
-        $elements['title'] = $this->createElement('text', 'title');
-        $elements['title']->setLabel('标题')
-                          ->setDescription('，你们有什么想问的？')
-                          ->addDecorator('Description', array('tag' => ''))
-                          ->setOptions(array('class' => 'xlarge'))
-                          ->setRequired(true);
-
-        $elements['content'] = $this->createElement('textarea', 'content');
-        $elements['content']->setLabel('内容')
-                            ->setOptions(array(
-                                'class' => 'xxlarge',
-                                'row'   => 3,
-                            ))
-                            ->setRequired(true);
+            $this->createElement('textarea', 'content')
+                 ->setLabel('内容')
+                 ->setOptions(array(
+                     'class' => 'xxlarge',
+                     'row'   => 3,
+                 ))
+                 ->setRequired(true),
         
-        $elements['submit'] = $this->createElement('submit', 'submit');
-        $elements['submit']->setLabel('提交')
-                           ->setOptions(array('class' => 'btn primary'))
-                           ->setIgnore(true)
-                           ->setOrder(100);
-
-        $this->addELements($elements);
+            $this->createElement('submit', 'submit')
+                 ->setLabel('提交')
+                 ->setOptions(array('class' => 'btn primary'))
+                 ->setIgnore(true)
+                 ->setOrder(100),
+        ));
     }
-
 }
 
--- application/forms/User/Base.php
+++ application/forms/User/Base.php
@@ -13,54 +13,51 @@ class Application_Form_User_Base extends OOXX_Form
     {
         $this->setMethod('post');
 
-        $elements = array();
+        $this->addELements(array(
+            $this->createElement('text', 'email')
+                 ->setLabel('邮箱')
+                 ->setOptions(array('class' => 'xlarge'))
+                 ->addFilter('StringTrim')
+                 ->addValidator('EmailAddress')
+                 ->addValidator('StringLength', true, array(3, 128))
+                 ->setRequired(true),
 
-        $elements['email'] = $this->createElement('text', 'email');
-        $elements['email']->setLabel('邮箱')
-                          ->setOptions(array('class' => 'xlarge'))
-                          ->addFilter('StringTrim')
-                          ->addValidator('EmailAddress')
-                          ->addValidator('StringLength', true, array(3, 128))
-                          ->setRequired(true);
+            $this->createElement('password', 'password')
+                 ->setLabel('密码')
+                 ->setOptions(array('class' => 'xlarge'))
+                 ->setRequired(true),
 
-        $elements['password'] = $this->createElement('password', 'password');
-        $elements['password']->setLabel('密码')
-                             ->setOptions(array('class' => 'xlarge'))
-                             ->setRequired(true);
+            $this->createElement('password', 'password2')
+                 ->setLabel('确认密码')
+                 ->setOptions(array('class' => 'xlarge'))
+                 ->addValidator('identical', false, array(
+                     'token' => 'password'
+                 ))
+                 ->setRequired(true),
 
-        $elements['password2'] = $this->createElement('password', 'password2');
-        $elements['password2']->setLabel('确认密码')
-                              ->setOptions(array('class' => 'xlarge'))
-                              ->addValidator('identical', false, array(
-                                  'token' => 'password'
-                              ))
-                              ->setRequired(true);
+            $this->createElement('text', 'nickname')
+                 ->setLabel('名号')
+                 ->setOptions(array('class' => 'xlarge'))
+                 ->addFilter('StringTrim')
+                 ->addValidator('StringLength', true, array(
+                     1,
+                     16,
+                     'utf8'
+                 ))
+                 ->setRequired(true),
 
-        $elements['nickname'] = $this->createElement('text', 'nickname');
-        $elements['nickname']->setLabel('名号')
-                             ->setOptions(array('class' => 'xlarge'))
-                             ->addFilter('StringTrim')
-                             ->addValidator('StringLength', true, array(
-                                 1,
-                                 16,
-                                 'utf8'
-                             ))
-                             ->setRequired(true);
+            $this->createElement('textarea', 'introduction')
+                 ->setLabel('个人介绍')
+                 ->setOptions(array(
+                     'class'    => 'xlarge',
+                     'rows'     => 5,
+                 )),
 
-        $elements['introduction'] = $this->createElement('textarea', 'introduction');
-        $elements['introduction']->setLabel('个人介绍')
-                                 ->setOptions(array(
-                                     'class'    => 'xlarge',
-                                     'rows'     => 5,
-                                 ));
-
-        $elements['submit'] = $this->createElement('submit', 'submit');
-        $elements['submit']->setLabel('提交')
-                           ->setOptions(array('class' => 'btn primary'))
-                           ->setIgnore(true)
-                           ->setOrder(100);
-
-        $this->addELements($elements);
+            $this->createElement('submit', 'submit')
+                 ->setLabel('提交')
+                 ->setOptions(array('class' => 'btn primary'))
+                 ->setIgnore(true)
+                 ->setOrder(100),
+        ));
     }
-
 }
