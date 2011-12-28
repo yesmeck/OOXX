bb43b2d47e1ead52e732fe8cbd2d457d16658cfb
Meck <yesmeck@gmail.com>
2011-12-28 22:06:46 +0800

    Refactor new topic form

--- application/forms/Topic.php
+++ application/forms/Topic.php
@@ -1,22 +1,39 @@
 <?php
 
-class Application_Form_Topic extends Zend_Form
+class Application_Form_Topic extends OOXX_Form
 {
 
     public function init()
     {
         $this->setAction('/topic/new')
              ->setMethod('post')
-             ->setAttrib('id', 'new-topic-form')
-             ->setDisableLoadDefaultDecorators(true);
+             ->setAttrib('id', 'new-topic-form');
 
-        $this->setDecorators(array(
-            array('ViewScript', array('viewScript' => 'topic/_form.phtml')),
-            'Form'
-        ));
+        $elements = array();
+
+        $elements['title'] = $this->createElement('text', 'title');
+        $elements['title']->setLabel('标题')
+                          ->setDescription('，你们有什么想问的？')
+                          ->addDecorator('Description', array('tag' => ''))
+                          ->setOptions(array('class' => 'xlarge'))
+                          ->setRequired(true);
+
+        $elements['content'] = $this->createElement('textarea', 'content');
+        $elements['content']->setLabel('内容')
+                            ->setOptions(array(
+                                'class' => 'xxlarge',
+                                'row'   => 3,
+                            ))
+                            ->setRequired(true);
         
-    }
+        $elements['submit'] = $this->createElement('submit', 'submit');
+        $elements['submit']->setLabel('提交')
+                           ->setOptions(array('class' => 'btn primary'))
+                           ->setIgnore(true)
+                           ->setOrder(100);
 
+        $this->addELements($elements);
+    }
 
 }
 
